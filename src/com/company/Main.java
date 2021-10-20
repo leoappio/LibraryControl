80f30e86d4413fb4ab9bda9e16f0a9f571e94d88
Leonardo <leonardo.appio@gmail.com>
2021-10-20 12:02:57 -0300

    métodos de insert inseridos na classe do banco de dados

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import com.company.db.Database;
+import com.company.models.User;
+
+import java.sql.SQLException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws SQLException {
+        
     }
 }
--- src/com/company/db/Database.java
+++ src/com/company/db/Database.java
@@ -1,4 +1,40 @@
 package com.company.db;
+import com.company.models.Loan;
+import com.company.models.Publication;
+import com.company.models.User;
+
+import java.sql.*;
 
 public class Database {
+    private static Connection connection;
+    static {
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:DatabaseLibrary.db");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Statement statement;
+    static {
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void insertUser(User user) throws SQLException {
+        String sqlQuery = "INSERT INTO USER(REGISTRATION, NAME) VALUES ('"+user.registration+"','"+user.name+"')";
+        statement.execute(sqlQuery);
+    }
+    public static void insertPublication(Publication publication) throws SQLException {
+        String sqlQuery = "INSERT INTO PUBLICATION(TITLE, AUTHOR, QUANTITY) VALUES ('"+publication.title+"','"+publication.author+"','"+publication.quantity+"')";
+        statement.execute(sqlQuery);
+    }
+    public static void insertLoan(Loan loan) throws SQLException {
+        String sqlQuery = "INSERT INTO LOAN(USERID, PUBLICATIONID, LATEDAYS, ISRETURNED) VALUES ('"+loan.userId+"','"+loan.publicationId+"','"+loan.lateDays+"','"+loan.isReturned+"')";
+        statement.execute(sqlQuery);
+    }
+
 }
